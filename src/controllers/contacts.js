8e2499865a1d8ada5b33243fd509e5b500f41cf2
inna_mishchenko <mishhenko.inna@gmail.com>
2025-04-30 14:27:20 +0200

    new branch

---
+++ src/controllers/contacts.js
@@ -0,0 +1,33 @@
+import { getContacts, getContactById } from "../services/contacts.js";
+
+export async function getAllContactsController(req, res, next) {
+  try {
+    const contacts = await getContacts();
+    res.status(200).json({
+      status: 200,
+      message: "Successfully found contacts!",
+      data: contacts,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getContactByIdController(req, res, next) {
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      return res.status(404).json({ message: "Contact not found" });
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import pino from "pino-http";
 import cors from "cors";
 import getEnvVar from "./utils/getEnvVar.js";
-import { getContactById, getContacts } from "./services/contacts.js";
+import contactsRouter from "./routers/contacts.js";
 
 const PORT = Number(getEnvVar("PORT", "3000"));
 
@@ -19,39 +19,7 @@ async function setupServer() {
     })
   );
 
-  app.get("/contacts", async (req, res, next) => {
-    try {
-      const contacts = await getContacts();
-
-      res.status(200).json({
-        status: 200,
-        message: "Successfully found contacts!",
-        data: contacts,
-      });
-    } catch (error) {
-      next(error);
-    }
-  });
-
-  app.get("/contacts/:contactId", async (req, res, next) => {
-    try {
-      const { contactId } = req.params;
-      const contact = await getContactById(contactId);
-
-      if (!contact)
-        return res.status(404).json({
-          message: "Contact not found",
-        });
-
-      res.status(200).json({
-        status: 200,
-        message: `Successfully found contact with id ${contactId}!`,
-        data: contact,
-      });
-    } catch (error) {
-      next(error);
-    }
-  });
+  app.use("/contacts", contactsRouter);
 
   app.use((req, res) => {
     res.status(404).json({ message: "Not found" });
